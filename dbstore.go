b704e2693e45b77873c45899966bbc3c6a59ebcf
Min Khant Ko Ko <min.k.k.ko@frontiir.com>
2018-08-24 12:03:35 +0630

    2nd

--- dbstore.go
+++ dbstore.go
@@ -28,27 +28,26 @@ type dbStore struct {
 }
 
 func (store *dbStore) StorePop(getpop *Population) error {
-
-	res, err := store.db.Query("INSERT INTO test.population (code,population) VALUES('" + getpop.LocationCode + "','" + getpop.Population + "')")
+	res, err := store.db.Query("INSERT INTO test.population (code,population) VALUES(?,?)", getpop.LocationCode, getpop.Population)
 	res.Close()
 	return err
 }
 
 func (store *dbStore) StoreDis(getdis *Districts) error {
-	res, err := store.db.Query("INSERT INTO test.districts (en_name,code) VALUES('" + getdis.Names.EnglishName + "','" + getdis.Code + "')")
+	res, err := store.db.Query("INSERT INTO test.districts (en_name,code) VALUES(?,?)", getdis.Names.EnglishName, getdis.Code)
 	res.Close()
 	return err
 }
 
 func (store *dbStore) StoreTown(gettown *Townships) error {
-	res, err := store.db.Query("INSERT INTO test.townships (tcode,dcode,en_name) VALUES('" + gettown.Code + "','" + gettown.DistrictCode + "','" + gettown.Names.EnglishName + "')")
+	res, err := store.db.Query("INSERT INTO test.townships (tcode,dcode,en_name) VALUES(?,?,?)", gettown.Code, gettown.DistrictCode, gettown.Names.EnglishName)
 	res.Close()
 	return err
 }
 
 func (store *dbStore) GetDistrictCode(name string) string {
 	var code string
-	res, _ := store.db.Query("SELECT code from test.districts WHERE en_name='" + name + "'")
+	res, _ := store.db.Query("SELECT code from test.districts WHERE en_name=?", name)
 	for res.Next() {
 		res.Scan(&code)
 	}
@@ -59,8 +58,7 @@ func (store *dbStore) GetDistrictCode(name string) string {
 func (store *dbStore) GetTownshipCodes(dtcode string) []string {
 	var tcode string
 	var tcodes []string
-
-	res, _ := store.db.Query("SELECT tcode from test.townships WHERE dcode='" + dtcode + "'")
+	res, _ := store.db.Query("SELECT tcode from test.townships WHERE dcode=?", dtcode)
 	for res.Next() {
 		res.Scan(&tcode)
 		tcodes = append(tcodes, tcode)
@@ -72,7 +70,7 @@ func (store *dbStore) GetTownshipCodes(dtcode string) []string {
 func (store *dbStore) GetTownshipNames(dtcode string) []string {
 	var tname string
 	var tnames []string
-	res, _ := store.db.Query("SELECT en_name from test.townships WHERE dcode='" + dtcode + "'")
+	res, _ := store.db.Query("SELECT en_name from test.townships WHERE dcode=?", dtcode)
 	for res.Next() {
 		res.Scan(&tname)
 		tnames = append(tnames, tname)
@@ -84,7 +82,7 @@ func (store *dbStore) GetTownshipNames(dtcode string) []string {
 
 func (store *dbStore) GetPopulation(code string) string {
 	var population string
-	res, _ := store.db.Query("SELECT population from test.population WHERE code='" + code + "'")
+	res, _ := store.db.Query("SELECT population from test.population WHERE code=?", code)
 	for res.Next() {
 		res.Scan(&population)
 	}
@@ -94,7 +92,7 @@ func (store *dbStore) GetPopulation(code string) string {
 
 func (store *dbStore) GetTownCodeByTownName(name string) string {
 	var tcode string
-	res, _ := store.db.Query("SELECT tcode from test.townships WHERE en_name='" + name + "'")
+	res, _ := store.db.Query("SELECT tcode from test.townships WHERE en_name=?", name)
 	for res.Next() {
 		res.Scan(&tcode)
 	}
@@ -104,7 +102,7 @@ func (store *dbStore) GetTownCodeByTownName(name string) string {
 
 func (store *dbStore) GetDistrictCodeByTownName(name string) string {
 	var dcode string
-	res, _ := store.db.Query("SELECT dcode from test.townships WHERE en_name='" + name + "'")
+	res, _ := store.db.Query("SELECT dcode from test.townships WHERE en_name=?", name)
 	for res.Next() {
 		res.Scan(&dcode)
 	}
